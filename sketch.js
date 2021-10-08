1023001ec0c271108243ed7318ce0bf9b17ee45e
harrrysio <72060634+harrrysio@users.noreply.github.com>
2021-10-07 19:51:39 -0700

    asda

---
+++ sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+  // put setup code here
+  createCanvas(100,250);
+  background(153);
+  c = color(random(0,255),random(0,255),random(0,255));
+  fill(c);
+  circle(30,20,30);
+  c = color(random(0,255),random(0,255),random(0,255));
+  fill(c);
+  circle(30,60,30);
+  c = color(random(0,255),random(0,255),random(0,255));
+  fill(c);
+  circle(30,100,30);
+  c = color(random(0,255),random(0,255),random(0,255));
+  fill(c);
+  circle(30,140,30);
+}
+
+function draw() {
+  // put drawing code here
+
+
+}
